69409e43b8321bd01c14b0f8660cb8595062b9e9
shunsuke <wos.shun@gmail.com>
2021-06-02 17:41:45 +0900

    review

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
     public function category() {
         return $this->belongsTo('App\Category');
     }
+
+    public function reviews() {
+        return $this->hasMany('App\Review');
+    }
 }
