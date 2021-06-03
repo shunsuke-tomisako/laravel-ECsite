50e4d859cdda11e2ae0e79c27996eb6444dc414b
shunsuke <wos.shun@gmail.com>
2021-06-03 18:28:52 +0900

    fix to deploy

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::resource('products', 'ProductController');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
