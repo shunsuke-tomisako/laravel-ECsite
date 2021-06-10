322574daed8c14491ff11a51b1fe498fad8902f7
shunsuke <wos.shun@gmail.com>
2021-06-10 22:15:57 +0900

    earnings

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -108,14 +108,41 @@ class CartController extends Controller
      */
     public function destroy(Request $request)
     {
-        $user_shoppingcarts = DB::table('shoppingcart')->where('instance', Auth::user()->id)->get();
+        $user_shoppingcarts = DB::table('shoppingcart')->get();
+        $number = DB::table('shoppingcart')->where('instance', Auth::user()->id)->count();
 
         $count = $user_shoppingcarts->count();
 
         $count += 1;
-        Cart::instance(Auth::user()->id)->store;
+        $number += 1;
+        $cart = Cart::instance(Auth::user()->id)->content();
 
-        DB::table('shoppingcart')->where('instance', Auth::user()->id)->where('number', null)->update(['number' => $count, 'buy_flag' => true]);
+        $price_total = 0;
+        $qty_total = 0;
+
+        foreach ($cart as $c) {
+            if ($c->options->carriage) {
+                $price_total += ($c->qty * ($c->price + 800));
+            } else {
+                $price_total += $c->qty * $c->price;
+            }
+            $qty_total += $c->qty;
+        }
+
+        Cart::instance(Auth::user()->id)->store($count);
+
+        DB::table('shoppingcart')->where('instance', Auth::user()->id)
+                                ->where('number', null)
+                                ->update(
+                                    [
+                                        'code' => substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10),
+                                        'number' => $number, 
+                                        'price_total' => $price_total,
+                                        'qty' => $qty_total,
+                                        'buy_flag' => true, 
+                                        'updated_at' => date("Y/m/d H:i:s")
+                                    ]
+                                );
 
         Cart::instance(Auth::user()->id)->destroy();
 
